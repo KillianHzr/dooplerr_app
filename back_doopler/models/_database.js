0215e136b959964c33e145cd0389e006920f3dd1
KillianHzr <killianherzer@gmail.com>
2024-03-08 11:41:01 +0100

    UPD [back] Database .env port

--- back_doopler/models/_database.js
+++ back_doopler/models/_database.js
@@ -6,8 +6,8 @@ const sequelizeInstance = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localhost', 
-        port: 8889,
+        host: process.env.DB_HOST, 
+        port: process.env.DB_PORT,
         dialect: 'mysql'
     }
 )
