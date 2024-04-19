b9714a9a6d7f212923e614e6c0404891500e0185
KillianHzr <killianherzer@gmail.com>
2024-04-19 11:15:00 +0200

    ADD [front] Init nuxtjs front

---
+++ front_dooplerr/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h1 class="text-violet-one underline">Dooplerr</h1>
+    </div>
+</template>
+
+<script setupe>
+
+</script>
---
+++ front_dooplerr/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        bleu: {
+          one: '#1D2549',
+          two: '#252C5C',
+          three: '#323D75'
+        },
+        violet: {
+          one: '#6735B7',
+          two: '#3E226B'
+        },
+        snow: '#FDFDFF'
+      }
+    },
+  },
+  plugins: [],
+}
+
