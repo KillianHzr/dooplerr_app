b00a83951b0d592990d068e03e624a1c0a1cae30
KillianHzr <killianherzer@gmail.com>
2024-05-31 13:58:03 +0200

    ADD [search] Recherche podcasts

--- front_dooplerr/pages/index.vue
+++ front_dooplerr/pages/index.vue
@@ -5,6 +5,15 @@
       <h2 class="text-3xl font-bold text-white mb-10">
         Les derniers podcasts
       </h2>
+      <div class="mb-4">
+        <input
+          v-model="searchQuery"
+          @keyup.enter="goToPodcasts"
+          type="text"
+          placeholder="Rechercher des podcasts..."
+          class="w-full p-2 border border-gray-300 rounded"
+        />
+      </div>
       <Wrapper display="carrousel">
         <CardPodcast v-for="podcast in podcasts" :key="podcast.id" :podcast="podcast"
           class="group relative aspect-video h-96 min-w-[300px] cursor-pointer overflow-hidden rounded-xl snap-start first:ps-6 w-full">
@@ -18,23 +27,31 @@
         </CardEpisode>
       </Wrapper>
     </div>
-
   </div>
 </template>
 
 <script setup>
-
 import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
 const { getPodcasts } = usePodcasts();
 const { getEpisodes } = useEpisodes();
 
 const podcasts = ref([]);
 const episodes = ref([]);
+const searchQuery = ref('');
+const router = useRouter();
 
 onMounted(async () => {
   podcasts.value = await getPodcasts();
   episodes.value = await getEpisodes();
 });
 
+const goToPodcasts = () => {
+  if (searchQuery.value) {
+    router.push({ path: '/podcasts', query: { search: searchQuery.value } });
+  } else {
+    router.push('/podcasts');
+  }
+};
 </script>
--- front_dooplerr/pages/podcasts/index.vue
+++ front_dooplerr/pages/podcasts/index.vue
@@ -1,8 +1,82 @@
 <template>
-    <div>
-      <TopNav />
+  <div class="px-5">
+    <section class="w-full py-10 flex flex-col">
+        <div class="flex justify-between">
+            <NuxtLink to="/" active-class="active-link" aria-label="Accueil">
+                <img src="~/assets/img/logo-dooplerr.png">
+            </NuxtLink>
+            <div class="flex gap-6">
+                <NuxtLink to="/profile" active-class="active-link" aria-label="Profil">
+                    <Icon name="material-symbols:account-circle-full" size="27" />
+                </NuxtLink>
+                <NuxtLink to="/messages" active-class="active-link" aria-label="Messages">
+                    <Icon name="akar-icons:send" size="27" />
+                </NuxtLink>
+            </div>
+        </div>
+        <div class="w-full">
+            <div class="relative py-5">
+                <input
+                  v-model="searchQuery"
+                  @input="filterPodcasts"
+                  type="text"
+                  class="w-full py-3 pl-11 pr-4 rounded-dooplerr border-transparent bg-dooplerr-grey focus:outline-none focus:border-indigo-400 text-white"
+                  placeholder="Un nouveau podcast ?">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <Icon name="mingcute:search-line" size="20" class="text-dooplerr-grey-purple" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container mx-auto">
+      <h2 class="text-3xl font-bold text-white mb-10">
+        Tous les podcasts
+      </h2>
+      <div class="list-content mb-24 text-white">
+        <PodcastCard
+          v-for="podcast in filteredPodcasts"
+          :key="podcast.id"
+          :id="podcast.id"
+          :thumbnail_path="podcast.thumbnail_path"
+          :title="podcast.title"
+          :description="podcast.description"
+          :commentsCount="podcast.commentsCount"
+          :isPublic="podcast.isPublic"
+          class="mt-6"
+        />
+      </div>
     </div>
-  </template>
-  
-  <script setup>  
-  </script>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { usePodcasts } from '~/composables/usePodcasts';
+
+const { getPodcasts } = usePodcasts();
+const podcasts = ref([]);
+const searchQuery = ref('');
+const filteredPodcasts = ref([]);
+const route = useRoute();
+
+onMounted(async () => {
+  podcasts.value = await getPodcasts();
+  searchQuery.value = route.query.search || '';
+  filterPodcasts();
+});
+
+const filterPodcasts = () => {
+  if (searchQuery.value) {
+    filteredPodcasts.value = podcasts.value.filter(podcast =>
+      podcast.title.toLowerCase().includes(searchQuery.value.toLowerCase())
+    );
+  } else {
+    filteredPodcasts.value = podcasts.value;
+  }
+};
+
+watch([searchQuery, () => route.query.search], () => {
+  filterPodcasts();
+});
+</script>
