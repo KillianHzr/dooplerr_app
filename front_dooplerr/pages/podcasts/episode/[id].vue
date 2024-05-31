3e32ad6e7b65451b24135092c984b3cdc34c982b
KillianHzr <killianherzer@gmail.com>
2024-05-31 19:10:30 +0200

    refactor: Update podcast card layout and styles

--- front_dooplerr/pages/podcasts/episode/[id].vue
+++ front_dooplerr/pages/podcasts/episode/[id].vue
@@ -31,8 +31,9 @@
           </div>
           
           <!-- Bouton de like -->
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21.29l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.47L12 21.29z" />
+          <svg @click="toggleLike" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" :class="{'fill-current text-dooplerr-purple-light': isLiked, 'text-white': !isLiked}" viewBox="0 0 24 24" stroke="currentColor">
+            <path v-if="isLiked" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21.29l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.47L12 21.29z" />
+            <path v-else stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21.29l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.47L12 21.29z" fill="none" />
           </svg>
         </div>
       </div>
@@ -143,6 +144,7 @@ const progressDotPosition = ref(0);
 const newComment = ref("");
 const isLoading = ref(true); 
 const showFullDescription = ref(false);
+const isLiked = ref(false);  // State for like button
 
 const audio = ref(null);
 
@@ -343,6 +345,11 @@ const randomColor = computed(() => {
   }
   return color;
 });
+
+// Toggle like button
+const toggleLike = () => {
+  isLiked.value = !isLiked.value;
+};
 </script>
 
 <style scoped>
